956edf519ada11751de4d4b6b42f1ecf5f4bae3a
adrgimmun <adrgimmun@PRN5-10365702>
2026-02-02 21:06:33 +0100

    Implementacion de generacion de archivo XML mediante DOM

--- src/VentanaAnimales.java
+++ src/VentanaAnimales.java
@@ -56,6 +56,11 @@ public class VentanaAnimales extends javax.swing.JFrame {
         });
 
         btnGenerar.setText("Generar XML");
+        btnGenerar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGenerarActionPerformed(evt);
+            }
+        });
 
         txtLista.setColumns(20);
         txtLista.setRows(5);
@@ -120,6 +125,37 @@ public class VentanaAnimales extends javax.swing.JFrame {
          javax.swing.JOptionPane.showMessageDialog(this, "Introduce un nombre.");
         }
     }//GEN-LAST:event_btnAgregarActionPerformed
+
+    private void btnGenerarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGenerarActionPerformed
+        // TODO add your handling code here:
+      if (animales.size() != 5) {
+        javax.swing.JOptionPane.showMessageDialog(this, "Debes introducir exactamente cinco animales.");
+        return;
+       }
+       try {
+        javax.xml.parsers.DocumentBuilderFactory factory =
+       javax.xml.parsers.DocumentBuilderFactory.newInstance();
+        javax.xml.parsers.DocumentBuilder builder = factory.newDocumentBuilder();
+        org.w3c.dom.Document doc = builder.newDocument();
+        org.w3c.dom.Element raiz = doc.createElement("animales");
+        doc.appendChild(raiz);
+        for (String a : animales) {
+        org.w3c.dom.Element nodoAnimal = doc.createElement("animal");
+        nodoAnimal.setTextContent(a);
+        raiz.appendChild(nodoAnimal);
+        }
+        javax.xml.transform.TransformerFactory tf = javax.xml.transform.TransformerFactory.newInstance();
+        javax.xml.transform.Transformer transformer = tf.newTransformer();
+        javax.xml.transform.dom.DOMSource origen = new javax.xml.transform.dom.DOMSource(doc);
+        java.io.File archivo = new java.io.File("animales.xml");
+        javax.xml.transform.stream.StreamResult destino = new
+       javax.xml.transform.stream.StreamResult(archivo);
+        transformer.transform(origen, destino);
+        javax.swing.JOptionPane.showMessageDialog(this, "XML generado correctamente.");
+       } catch (Exception ex) {
+        ex.printStackTrace();
+       }  
+    }//GEN-LAST:event_btnGenerarActionPerformed
 
     /**
      * @param args the command line arguments
