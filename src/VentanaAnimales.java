0fc2a20ddea39e026976396372f0552977f99437
adrgimmun <adrgimmun@PRN5-10365702>
2026-02-02 21:03:26 +0100

    Implementación de lista en memoria y lógica del botón Agregar

--- src/VentanaAnimales.java
+++ src/VentanaAnimales.java
@@ -36,7 +36,7 @@ public class VentanaAnimales extends javax.swing.JFrame {
         btnAgregar = new javax.swing.JButton();
         btnGenerar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        txtLista = new javax.swing.JTextField();
+        txtLista = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -57,11 +57,8 @@ public class VentanaAnimales extends javax.swing.JFrame {
 
         btnGenerar.setText("Generar XML");
 
-        txtLista.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtListaActionPerformed(evt);
-            }
-        });
+        txtLista.setColumns(20);
+        txtLista.setRows(5);
         jScrollPane1.setViewportView(txtLista);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -90,14 +87,14 @@ public class VentanaAnimales extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(txtAnimal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 224, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnAgregar)
                     .addComponent(btnGenerar))
                 .addGap(15, 15, 15))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -107,10 +104,6 @@ public class VentanaAnimales extends javax.swing.JFrame {
     private void txtAnimalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtAnimalActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtAnimalActionPerformed
-
-    private void txtListaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtListaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtListaActionPerformed
 
     private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
         // TODO add your handling code here:
@@ -159,6 +152,6 @@ public class VentanaAnimales extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField txtAnimal;
-    private javax.swing.JTextField txtLista;
+    private javax.swing.JTextArea txtLista;
     // End of variables declaration//GEN-END:variables
 }
